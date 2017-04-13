e506f92effddd60eca7b66bb4579bc782393e4cf
Lars Vatne <lars.vatne@gmail.com>
2017-04-13 21:37:22 +0000

    Rewrote the Tracker() class to tidy up the inertial sensor readings. Still to do: Write the PID control systems for distance and rotation

--- src/platform/tracker.py
+++ src/platform/tracker.py
@@ -15,10 +15,38 @@ import sysprops
 
 class Tracker:
     """ Tracker class: Handles communication with on-board inertial
-    sensors and implements the PID regulator that maintains a straight course.
+    sensors and implements the PID regulators for maintaining a straight course,
+    roll the desired distance and rotate the desired number of degrees.
     """
     def __init__(self):
-        self.BUFSZ = 100
+        self.BUFSZ = 5
+        self.inertial_data = np.zeros((self.BUFSZ, 22), dtype=float)
+        # Accessors for the inerial_data cells
+        self.tstamp  = 0
+        self.acc_x   = 1
+        self.acc_y   = 2
+        self.acc_z   = 3
+        self.gyr_x   = 4
+        self.gyr_y   = 5
+        self.gyr_z   = 6
+        self.angle_x = 7   # Number of degrees turned since previous sample
+        self.angle_y = 8
+        self.angle_z = 9
+        self.vel_x   = 10
+        self.vel_y   = 11
+        self.vel_z   = 12
+        self.steer_P = 13
+        self.steer_I = 14
+        self.steer_D = 15
+        self.dist_P  = 16
+        self.dist_I  = 17
+        self.dist_D  = 18
+        self.rot_P   = 19
+        self.rot_I   = 20
+        self.rot_D   = 21
+        self.first_run = True
+        
+        #
         self.PV_buf = np.zeros(self.BUFSZ, dtype=float) # PV values for the PID regulator
         self.P_buf = np.zeros(self.BUFSZ, dtype=float) # P values for PID regulator
         self.I_buf = np.zeros(self.BUFSZ, dtype=float) # I values for the PID regulator
@@ -27,10 +55,9 @@ class Tracker:
         self.CO_buf = np.zeros(self.BUFSZ, dtype=float) # Controller Output from the PID regulator
         self.gyro_buf = np.zeros(self.BUFSZ, dtype=float) # raw gyro values
         self.acc_buf = np.zeros(self.BUFSZ, dtype=float) # raw accleleration values
-        self.tstamp = np.zeros(self.BUFSZ, dtype=float) # Time stamps for samples
-        self.Kp = 5
-        self.Ki = 2
-        self.Kd = 1
+        self.Kp_steer = 5
+        self.Ki_steer = 2
+        self.Kd_steer = 1
         
         self.buf_idx = 0 # We're at the same place in all buffers
         self.Fs = 50.0 # sample rate, Hz
@@ -68,6 +95,7 @@ class Tracker:
         In case the robot is stuck or similar, various methods will be employed to
         free the wheels or whatever
         """
+        self.inertial_data[0, self.steer_P] = 0.0
         recover_attempts = 0
         retval = self.move_dist_basic(meter)
         while retval > 0 and recover_attempts < 5:
@@ -149,10 +177,10 @@ class Tracker:
                     self.m.signal(self.m.RUN_FWD, power)
                 else:
                     self.m.signal(self.m.RUN_REV, power)
-                adj_accel_data, adj_gyro_data = self.keep_running(x_offset, setpoint)
+                adj_accel_data, adj_gyro_data = self.get_inertial_measurements(x_offset, setpoint)
                 vel = vel + adj_accel_data * self.Ta
                 r_dist = self.remaining_dist(vel, prev_vel, r_dist)
-                PID_val = self.PID(setpoint)
+                PID_val = self.PIDsteer(setpoint)
                 # print("m_fwd: vel: %3.3f, r_dist: %3.3f, PID: %3.3f" % (vel, r_dist, PID_val))
                 if PID_val >= 0:
                     if PID_val > 100:
@@ -179,29 +207,27 @@ class Tracker:
         else:
             return 0
 
-    def PID(self, setpoint):
+    def PIDsteer(self, setpoint):
         """ Calculate the error value to apply to the steering using P_ I_ and D_buf class buffers.
         The buffer index has already been incremented, so go back one.
         Return a Controler Output (CO) in the range -100 to +100
         """
-        errval = 0.0
-        idx = self.buf_idx - 1
-        if self.buf_idx > 1:
-            errval = self.E_buf[idx] * self.Kp
-            # Sum up the I values
-            Ival = 0.0
-            if idx > 10:
-                for i in range(10):
-                    Ival = Ival + self.I_buf[idx - i]
-            else:
-                for i in range(idx):
-                    Ival = Ival + self.I_buf[i]
-            errval = errval + Ival * self.Ki
-            errval = errval + self.D_buf[idx] * self.Kd
-            # if self.dbg_cnt % 25 == 0:
-            # print("Gyro: %3.1f Steer: %3.1f PIDraw: %3.1f %3.1f %3.1f  PIDgain: %3.1f %3.1f %3.1f tstamp: %f" %
-            #      (self.gyro_buf[idx], setpoint - errval, self.E_buf[idx], Ival, self.D_buf[idx], self.E_buf[idx] * self.Kp, Ival * self.Ki, self.D_buf[idx] *self.Kd, time.time()))
-                
+        # Pre calculate values for the PID regulator
+        P = self.inertial_data[1, self.steer_P] + self.inertial_data[0, self.angle_z]
+        max_p = max(self.inertial_data[0, self.steer_P], self.inertial_data[1, self.steer_P])
+        min_p = min(self.inertial_data[0, self.steer_P], self.inertial_data[1, self.steer_P])
+        I = (max_p - min_p) / 2 * self.Ta + min_p * self.Ta
+        D = self.inertial_data[0, self.gyr_z]
+
+        self.inertial_data[0, self.steer_P] = P
+        self.inertial_data[0, self.steer_I] = I
+        self.inertial_data[0, self.steer_D] = D
+        
+        errval = P * self.Kp_steer
+
+        errval = errval + I * self.Ki_steer
+        errval = errval + D * self.Kd_steer
+
         CO = setpoint - errval
         if CO < -100:
             CO = -100.0
@@ -209,70 +235,52 @@ class Tracker:
             CO = 100.0
         return CO
             
-    def keep_running(self, x_offset, setpoint):
-        """ Sample data and prepare the values for the PID regulator.
+    def get_inertial_measurements(self):
+        """ Sample data from the inertial navigation sensors and prepare the values for the PID regulators.
         Acceleration data and gyro data is sampled at Fs
         Operates directly on the class buffers: acc_buf, gyro_buf, P_buf, I_buf and D_buf
         """
-        
         if self.Ta < self.Ts:
             time.sleep(self.Ts - self.Ta) # maintain sampling rate
-        idx = self.buf_idx    
+        # Rotate the inertial_data array so that index 0 is ready to accept new values
+        for i in range(self.BUFSZ - 1, 0, -1):
+            self.inertial_data[i] = self.inertial_data[i - 1]
+
+        # Read the raw data from the inertial navigation sensors
         acc = self.a.getAxes()
-        self.acc_buf[idx] = acc[0] - self.x_offset # Apply calibration
         gyr = self.g.read()
-        self.tstamp[idx] = time.time()
-        instant_gyro = gyr[2] # It's the Z rotation we're using
-        if idx > 0:
-            self.Ta = self.tstamp[idx] - self.tstamp[idx - 1]
-            if self.Ta < 0.0:
-                self.Ta = math.fabs(self.Ta)
-            self.gyro_buf[idx] = self.gyro_buf[idx - 1] + instant_gyro * self.Ta
-        else:
-            # self.Ta = self.Ts
-            self.gyro_buf[idx] = instant_gyro * self.Ta
-        # filter the data
-        if idx > 4:
-            # Hanning filter (moving average window of 5 with 2x weight on current value)
-            adj_accel = (self.acc_buf[idx-4] + self.acc_buf[idx-3] + self.acc_buf[idx-2] +
-                         self.acc_buf[idx-1] + 2 * self.acc_buf[idx]) / 6
-            # adj_gyro = (gyro_buf[idx-4] + gyro_buf[idx-3] + gyro_buf[idx-2] + gyro_buf[idx-1] + 2 * gyro_buf[idx]) / 6
-            adj_gyro = self.gyro_buf[idx] # Just using raw gyro value without filtering. Internal filter in chip
-        else:
-            adj_accel = self.acc_buf[idx]
-            adj_gyro = self.gyro_buf[idx]
+        t_now = time.time()
 
-        # Pre calculate values for the PID regulator
-        self.PV_buf[idx] = adj_gyro
-        if idx > 0:
-            self.E_buf[idx] = setpoint - adj_gyro # i.e. self.PV_buf[idx]
-            self.I_buf[idx] = (self.E_buf[idx] - self.E_buf[idx-1]) * (self.Ta / 2) + min(self.E_buf[idx], self.E_buf[idx-1]) * self.Ta
-            self.D_buf[idx] = (self.E_buf[idx]- self.E_buf[idx-1]) / self.Ta
-        else:
-            self.I_buf[idx] = 0.0
-            self.D_buf[idx] = 0.0
-            
-        if self.buf_idx < self.BUFSZ - 2:
-            self.buf_idx = self.buf_idx + 1
-        else:  # Move the last of the buffer down to the first 3 cells and wrap the index
-            self.buf_idx = 2
-            for i in range(3):
-                tgt_i = self.BUFSZ - 5 + i
-                self.E_buf[i] = self.E_buf[tgt_i]
-                self.I_buf[i] = self.I_buf[tgt_i]
-                self.D_buf[i] = self.D_buf[tgt_i]
-                self.tstamp[i] = self.tstamp[tgt_i]
+        # Store the most recent raw data at index 0 in the inertial_data buffer
+        self.inertial_data[0, self.tstamp] = t_now
+        self.inertial_data[0, self.acc_x] = acc[0]
+        self.inertial_data[0, self.acc_y] = acc[1]
+        self.inertial_data[0, self.acc_z] = acc[2]
+        self.inertial_data[0, self.gyr_x] = gyr[0]
+        self.inertial_data[0, self.gyr_y] = gyr[1]
+        self.inertial_data[0, self.gyr_z] = gyr[2]
 
-        # if self.dbg_cnt == 99:
-        #     np.savetxt("gyro_buf.txt", self.gyro_buf)
-        #     np.savetxt("PV_buf.txt", self.PV_buf)
-        #     np.savetxt("E_buf.txt", self.E_buf)
-        #     np.savetxt("I_buf.txt", self.I_buf)
-        #     np.savetxt("D_buf.txt", self.D_buf)
-        if self.dbg_cnt % 50 == 0:
-            self.logger.info("Ta = %3.5f Fa = %3.3f Hz" % (self.Ta, 1.0 / self.Ta))
+        # Calculate actual time elapsed since previous sample, Ta
+        if not self.first_run:
+            self.Ta = self.inertial_data[0, self.tstamp] - self.inertial_data[1, self.tstamp]
+        else:
+            self.Ta = 0.0
+            self.first_run = False
+        
+        instant_gyro = self.inertial_data[0, self.gyr_z] # It's the rotation about the Z axis we're using
+
+        self.inertial_data[0, self.angle_z] = instant_gyro * self.Ta
+        # Add rotation data for X and Y axis later if required.....
+        
+        self.inertial_data[0, self.vel_x] = self.inertial_data[1, self.vel_x] + self.inertial_data[0, self.acc_x] * self.Ta
+        self.inertial_data[0, self.vel_y] = self.inertial_data[1, self.vel_y] + self.inertial_data[0, self.acc_y] * self.Ta
+        self.inertial_data[0, self.vel_z] = self.inertial_data[1, self.vel_z] + self.inertial_data[0, self.acc_z] * self.Ta
+
+
+        # if self.dbg_cnt % 50 == 0:
+        #     self.logger.info("Ta = %3.5f Fa = %3.3f Hz" % (self.Ta, 1.0 / self.Ta))
         self.dbg_cnt = self.dbg_cnt + 1
-        return adj_accel, adj_gyro
+        # return adj_accel, adj_gyro
 
     def remaining_dist(self, velocity, prev_velocity, prev_distance):
         remaining = prev_distance - self.Ta * (prev_velocity + 0.5 * (velocity - prev_velocity))
@@ -364,7 +372,7 @@ class Tracker:
         giveup = 500
         idx = 1
         self.buf_reset()
-        self.tstamp[0] = time.time()
+        # self.tstamp[0] = time.time()
         self.gyro_buf[0] = 0.0
         rel_angle = rel_angle % 360
         if rel_angle > 180:
@@ -378,9 +386,9 @@ class Tracker:
             self.m.signal(self.m.TURN_LEFT, 100)
         while turn_angle > 0.5 or turn_angle < -0.5:
             gyr = self.g.read()
-            self.tstamp[idx] = time.time()
+            # self.tstamp[idx] = time.time()
             instant_gyro = gyr[2] # It's the Z rotation we're using. In deg/sec
-            self.Ta = self.tstamp[idx] - self.tstamp[idx - 1]
+            # self.Ta = self.tstamp[idx] - self.tstamp[idx - 1]
             if self.Ta < 0.0:
                 self.Ta = math.fabs(self.Ta)
             self.gyro_buf[idx] = self.gyro_buf[idx - 1] + instant_gyro * self.Ta
@@ -397,7 +405,7 @@ class Tracker:
                 self.m.signal(self.m.TURN_LEFT, power)
             idx = idx + 1
             if idx > self.BUFSZ - 2: # Wrap the buffers around
-                self.tstamp[0] = self.tstamp[idx - 1]
+                # self.tstamp[0] = self.tstamp[idx - 1]
                 self.gyro_buf[0] = self.gyro_buf[idx - 1]
                 idx = 1
             if self.Ta < self.Ts:
@@ -416,8 +424,8 @@ class Tracker:
         """ Adjust the relative power of the motors while running forward
         """
         for i in range(20):
-            adj_accel_data, adj_gyro_data = self.keep_running(0, angle)
-            self.m.steer(self.PID(angle))
+            adj_accel_data, adj_gyro_data = self.get_inertial_measurements(0, angle)
+            self.m.steer(self.PIDsteer(angle))
 
     def buf_reset(self):
         for i in range(5):
@@ -429,21 +437,21 @@ class Tracker:
             self.CO_buf[i] = 0.0 # Controller Output from the PID regulator
             self.gyro_buf[i] = 0.0 # raw gyro values
             self.acc_buf[i] = 0.0 # raw accleleration values
-            self.tstamp[i] = time.time() # Time stamps for samples
+            # self.tstamp[i] = time.time() # Time stamps for samples
 
     def dump_buf(self, filename):
         i = self.buf_idx
         j = 0
         dump_buf = np.zeros((self.BUFSZ, 3), dtype=float)
         while i < self.BUFSZ:
-            dump_buf[j, 0] = self.tstamp[i]
+            # dump_buf[j, 0] = self.tstamp[i]
             dump_buf[j, 1] = self.gyro_buf[i]
             dump_buf[j, 2] = self.acc_buf[i]
             j = j + 1
             i = i + 1
         i = 0
         while i < self.buf_idx:
-            dump_buf[j, 0] = self.tstamp[i]
+            # dump_buf[j, 0] = self.tstamp[i]
             dump_buf[j, 1] = self.gyro_buf[i]
             dump_buf[j, 2] = self.acc_buf[i]
             j = j + 1
@@ -481,8 +489,11 @@ class Gyro:
         return False
 
     def read(self):
-        # Read data back from 0x1D(29), 6 bytes
-        # X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB
+        """ Read data back from 0x1D(29), 6 bytes
+            X-Axis MSB, X-Axis LSB, Y-Axis MSB, Y-Axis LSB, Z-Axis MSB, Z-Axis LSB
+            Returns instantaneous rotation around the X, Y and Z axis in degrees per second.
+            """
+        
         data = self.bus.read_i2c_block_data(self.address, 0x1D, 6)
 
         # Convert the data
